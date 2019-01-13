521793951fc32db88d9530412ff0f33f5994cd1b
Julia Magyar <magyar.juliar@gmail.com>
2019-01-13 17:16:40 -0500

    almost done

--- liri.js
+++ liri.js
@@ -1,2 +1,60 @@
 require("dotenv").config();
-var keys = new Spotify(keys.spotify);
+var moment = require("moment");
+var keys = require("./keys.js");
+var request = require('request');
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
+var spotify = new Spotify(keys.spotify);
+
+
+//variable to hold user's command
+var command = process.argv[2];
+//loop to collect other info entered by user
+var userQuery = "";
+
+for(i=3; i < process.argv.length; i++){
+    if(i == 3){
+        userQuery = process.argv[i];
+    }
+    else{
+            userQuery += "+" + process.argv[i];
+    }
+    
+}
+
+
+
+//for user command concert-this
+if(command === "concert-this"){
+    axios.get("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp").then(
+        function(response){
+            
+            for(i=0; i < response.data.length; i++){
+                date = moment(response.data[i].datetime).format("L")
+                console.log("\nVenue: " + response.data[i].venue.name + "\nLocation: " + response.data[i].venue.city + ", " + response.data[i].venue.region + " " + response.data[i].venue.country + "\nDate: " + date)
+            }
+        }
+    )
+}
+
+//for command spotify-this-song
+if(command === "spotify-this-song"){
+    if(userQuery == ""){
+        userQuery = "the sign ace of base"
+    }
+    spotify.search({ type: 'track', query: userQuery }).then(
+        function(response){
+            console.log("\nArtist: " + response.tracks.items[0].artists[0].name + "\nSong: " + response.tracks.items[0].name + "\nPreview Link: " + response.tracks.items[0].preview_url + "\nAlbum name: " + response.tracks.items[0].album.name)
+        }
+    )
+}
+
+//for command movie this
+if(command === "movie-this"){
+    axios.get("http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=trilogy").then(
+        function(response) {
+            var path = response.data
+            console.log("\nTitle: " + path.Title + "\nRelease Year: " + path.Year + "\nIMDB rating: " + path.imdbRating + "\nRotten Tomatoes Rating: " + path.Ratings[1].Value + "\nCountry: " + path.Country + "\nLanguage: " + path.Language + "\nPlot: " + path.Plot + "\nActors: " + path.Actors)
+        }     
+    );
+}
